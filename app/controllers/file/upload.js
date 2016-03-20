a19454cd1ca92956582bfea3be25b73dc1053701
Enkey <fudo333@gmail.com>
2016-03-20 15:15:48 +0200

    +

--- app/controllers/file/upload.js
+++ app/controllers/file/upload.js
@@ -1,6 +1,23 @@
 var multer  = require('multer');
-var upload = multer({ dest: 'public/files/' }).single('image');
+var crypto = require('crypto');
+var path = require('path');
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/files/')
+    },
+    filename: function (req, file, cb) {
+        crypto.pseudoRandomBytes(16, function (err, raw) {
+            cb(null, raw.toString('hex') + Date.now() + path.extname(file.originalname));
+        });
+    }
+});
+
+var upload = multer({ storage: storage }).single('image');
 var File = require('../../models/file');
+
+var validMIMETypes = ['image/gif', 'image/jpeg', 'image/png', 'image/pjpeg'];
+
 
 module.exports = function (req, res, next) {
     upload(req, res, function (err) {
@@ -9,6 +26,10 @@ module.exports = function (req, res, next) {
             // An error occurred when uploading
             console.log(err);
             next();
+        }
+
+        if(validMIMETypes.indexOf(req.file.mimetype) == -1) {
+            return res.json({error: true, message: 'Invalid file type!'})
         }
 
         var file = new File({
