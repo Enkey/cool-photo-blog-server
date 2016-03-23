18128028fa86745d226ae95061e013a60ea81215
Enkey <fudo333@gmail.com>
2016-03-23 23:59:15 +0200

    posts list bu category

--- app/routes/posts.js
+++ app/routes/posts.js
@@ -4,5 +4,6 @@ var post = require('../controllers/posts');
 
 postRouter.post('/add', post.add);
 postRouter.get('/', post.main);
+postRouter.get('/category/:category_id', post.category);
 
 module.exports = postRouter;
